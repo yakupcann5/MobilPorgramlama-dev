73da89a346ae746960cdc12325131e34ebaf10ce
Ceyda <ceydaceyahan2001@gmail.com>
2023-12-30 18:25:18 +0300

    Revert "reminder_add design done"
    This reverts commit 78eff997ecfe5e1a5ec98f5fc903964902caf139.

--- app/src/main/java/com/mobilProgramlama/odev/common/Timer.kt
+++
@@ -1,8 +0,0 @@
-package com.mobilProgramlama.odev.common
-
-enum class Timer(val type: Int) {
-    TenMinute(10),
-    TwentyMinute(20),
-    ThirtyMinute(30),
-    FortyMinute(40),
-}
--- app/src/main/java/com/mobilProgramlama/odev/ui/reminder_add/ReminderAddActivity.kt
+++ app/src/main/java/com/mobilProgramlama/odev/ui/reminder_add/ReminderAddActivity.kt
@@ -2,33 +2,16 @@ package com.mobilProgramlama.odev.ui.reminder_add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.mobilProgramlama.odev.R
-import com.mobilProgramlama.odev.common.Timer
 import com.mobilProgramlama.odev.databinding.ActivityReminderAddBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ReminderAddActivity : AppCompatActivity(), View.OnClickListener {
+class ReminderAddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReminderAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReminderAddBinding.inflate(layoutInflater)
-        initView()
         setContentView(binding.root)
-    }
-
-    private fun initView() {
-        binding.saveButton.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            binding.saveButton.id -> {
-                //viewModel.checkInput()
-                //viewModel.insertReminder()
-                //viewModel.checkInsertReminderResult()
-            }
-        }
     }
 }
