4cd1ad80ee13d32ccce19e74fab14782a065a03a
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2024-01-07 17:07:54 +0300

    Tamamlanan alrmların listelenmesi tamamlandı

--- app/src/main/java/com/mobilProgramlama/odev/ui/completed_list/ComplatedListActivity.kt
+++ app/src/main/java/com/mobilProgramlama/odev/ui/completed_list/ComplatedListActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobilProgramlama.odev.R
 import com.mobilProgramlama.odev.databinding.ActivityComplatedListBinding
 import com.mobilProgramlama.odev.ui.main.MainActivityViewModel
+import com.mobilProgramlama.odev.ui.main.ReminderRecyclerViewAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ComplatedListActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityComplatedListBinding
+    private lateinit var completedListRecyclerViewAdapter: CompletedListRecyclerViewAdapter
     private val completeListActivityViewModel: CompleteListActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +22,25 @@ class ComplatedListActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityComplatedListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+        initRecyclerView()
         getAllCompleteReminderList()
     }
 
+    private fun initRecyclerView() {
+        completedListRecyclerViewAdapter = CompletedListRecyclerViewAdapter()
+        binding.completedListRc.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.completedListRc.adapter = completedListRecyclerViewAdapter
+
+    }
+
     private fun getAllCompleteReminderList() {
-        completeListActivityViewModel.getAllCompleteReminderList()
+        completeListActivityViewModel.getAllCompleteReminderList(System.currentTimeMillis())
+        completeListActivityViewModel.completeList.observe(this) {
+            it.let {
+                completedListRecyclerViewAdapter.submitList(it as ArrayList)
+            }
+        }
     }
 
     private fun init() {
--- app/src/main/java/com/mobilProgramlama/odev/ui/completed_list/CompletedListRecyclerViewAdapter.kt
+++ app/src/main/java/com/mobilProgramlama/odev/ui/completed_list/CompletedListRecyclerViewAdapter.kt
@@ -1,26 +1,29 @@
 package com.mobilProgramlama.odev.ui.completed_list
 
 import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.mobilProgramlama.odev.R
 import com.mobilProgramlama.odev.data.locale.entity.reminder.ReminderEntity
-import com.mobilProgramlama.odev.databinding.ReminderItemBinding
-import com.mobilProgramlama.odev.ui.main.ReminderRecyclerViewAdapter
+import com.mobilProgramlama.odev.databinding.CompletedListItemBinding
 
 class CompletedListRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var reminder: ArrayList<ReminderEntity> = arrayListOf()
 
-    class ViewHolder(view: ReminderItemBinding) :
+    class ViewHolder(view: CompletedListItemBinding) :
         RecyclerView.ViewHolder(view.root) {
-        val sentBinding: ReminderItemBinding = view
+        val sentBinding: CompletedListItemBinding = view
     }
 
-
-
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<CompletedListItemBinding>(
+            inflater,
+            R.layout.completed_list_item, parent, false
+        )
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -28,7 +31,7 @@ class CompletedListRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewH
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ReminderRecyclerViewAdapter.ViewHolder).sentBinding.reminder = reminder[position]
+        (holder as ViewHolder).sentBinding.reminder = reminder[position]
     }
 
     @SuppressLint("NotifyDataSetChanged")
