3ce5b77a0e3fc8ff475dfd9b1ff3c46ec0c5c691
ayseakbaba <ayse.akbaba@detaysoft.com>
2024-01-08 00:20:25 +0300

    reminderActivity.kt and activity_reminder.xml edit, app icon was set

--- app/src/main/java/com/mobilProgramlama/odev/ui/reminder/ReminderActivity.kt
+++ app/src/main/java/com/mobilProgramlama/odev/ui/reminder/ReminderActivity.kt
@@ -4,13 +4,15 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.mobilProgramlama.odev.databinding.ActivityReminderBinding
+import com.mobilProgramlama.odev.domain.model.reminder.ReminderModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ReminderActivity : AppCompatActivity() {
+class ReminderActivity : AppCompatActivity(){
     private lateinit var binding: ActivityReminderBinding
     private val deneme: ReminderViewModel by viewModels()
 
@@ -19,8 +21,10 @@ class ReminderActivity : AppCompatActivity() {
         binding = ActivityReminderBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getAllReminder()
+        init()
 
     }
+
 
     private fun getAllReminder() {
         deneme.getAllReminder(System.currentTimeMillis())
@@ -30,8 +34,25 @@ class ReminderActivity : AppCompatActivity() {
                     val mp: MediaPlayer = MediaPlayer.create(this, Uri.parse(it.sound))
                     Log.d("muhammed123123", "getAllReminder: ${it.sound}")
                     mp.start()
+                    binding.reminderAddButton2.setOnClickListener{
+                        mp.stop()
+                    }
                 }
             }
         }
     }
+
+    private fun init(){
+        binding.customToolbar.customToolbarOptionalButton.visibility = View.GONE
+        binding.customToolbar.customToolbarTitle.text = "Hatırlatma"
+        /*val reminderModel = ReminderModel() //??
+
+        binding.dateTextView.text = "${reminderModel.date}"
+        binding.timeTextView.text = "${reminderModel.time}"
+        binding.titleTextView.text = "${reminderModel.title}"
+        binding.descriptionTextView.text = "${reminderModel.description}"
+
+         */
+    }
+
 }
